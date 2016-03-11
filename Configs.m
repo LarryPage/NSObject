05f61b04d4a6d8eade7fb0226c33af1f758eb21b
sbtjfdn <sbtjfdn@hotmail.com>
2016-03-11 18:58:12 +0800

    V3.3
    Signed-off-by: sbtjfdn <sbtjfdn@hotmail.com>

--- Configs.m
+++ Configs.m
@@ -8,52 +8,6 @@
 
 #import "Configs.h"
 
-//api text:
-#ifndef DEBUG
-NSString *APIServer = @"http://api.huijiame.com"; // release版本:
-#else
-NSString *APIServer = @"http://api.huijiame.com"; // debug版本:
-#endif
-
-NSString *FileServer = @"img.peng.me";//@"file.peng.me"
-Boolean AllowCompressedResponse=YES;
-
-// baiduMobStat app key ID
-NSString *baiduMobStatAPPKey = @"09088c50e3";
-NSString *baiduMobStatChannelID = @"App Store";//@"91store";@"Cydia";//自动判断:[UIDevice isJailbroken]?@"91store":@"App Store";
-
-// WeiXin app key ID
-NSString *weixinAppID = @"wx49bc895bdb3ae540";//与URL type->URL scheme中的相同
-NSString *weixinAppKey = @"5aaba490an90b451ca624byfbcgd82ye";//暂时没用
-
-//Sina　app key ID　//建议用网页应用，客户端应用只限于手机客端
-NSString *SINA_APP_KEY = @"244948476";
-NSString *SINA_APP_SECRET = @"edb3252033258d8187dcd340bba86faa";
-
-//Tencent　app key ID //建议用网页应用，客户端应用只限于手机客端
-NSString *Tencent_APP_KEY = @"801307650";
-NSString *Tencent_APP_SECRET = @"ae36f4ee3946e1cbb98d6965b0b2ff5c";
-
-//TencentOpen　app key ID //
-NSString *TencentOpen_APP_KEY = @"100839155";
-NSString *TencentOpen_APP_SECRET = @"321079098cfd2e21ab1240a2ff871ee7";
-
-// LinkedIn
-NSString *LinkedIn_APP_KEY = @"1zz3317i8jfj";
-NSString *LinkedIn_APP_SECRET = @"xmZ3PRvaklkfQRJa";
-
-// Renren
-NSString *Rerren_APP_KEY = @"241477";
-NSString *Rerren_APP_SECRET = @"619089569c6e471982843b043137644d";
-
-//ShareSDK　app key ID
-NSString *ShareSDK_APP_KEY = @"63b2f07d620";
-
-//Qiniu app key id & Bucket Name
-NSString *QiniuAccessKey = @"hJpfByk2kUiad2O8JJzd06W5w9njcvFuzuBYmsxE";
-NSString *QiniuSecretKey = @"KyieT2GIkb7GqyYAA59hdGl46BZoig7bo8kQdBa4";
-NSString *QiniuBucketNameMsgPic = @"rrlt-image-msg";
-
 @implementation Configs
 
 - (id)init
